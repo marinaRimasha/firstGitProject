7b1d9c853407db7b60b1cd50a10f0703b12b2761
marinaRimasha <rimasa.marina@gmail.com>
2024-04-03 20:53:58 +0300

    added first files

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
-const sayHelloLinting = (fName) => {console.log(`Look, how pretty it is, ${fName}`);};
+const sayHelloLinting = (fName) => {
+  console.log(`Look, how pretty it is, ${fName}`);
+};
 
-sayHelloLinting('Oksana');
+sayHelloLinting("Oksana");
